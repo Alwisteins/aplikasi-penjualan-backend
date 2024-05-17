ef6ec87ef48f42fbfaa830510fcd745103c72e15
unknown <alwyyourbae@gmail.com>
2024-05-17 22:58:18 +0700

    feat: add deleteSaleById

--- src/model/salesModel.js
+++ src/model/salesModel.js
@@ -9,8 +9,10 @@ const updateSaleById = (id, filteredData) => {
   return prisma.sales.update({ where: { id }, data: filteredData });
 };
 
+const deleteSaleById = (id) => prisma.sales.delete({ where: { id } });
+
 const getAllSales = () => prisma.sales.findMany();
 
-const salesModel = { createSale, updateSaleById, getAllSales };
+const salesModel = { createSale, updateSaleById, deleteSaleById, getAllSales };
 
 export default salesModel;
