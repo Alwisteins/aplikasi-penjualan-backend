e539c07f056625e6f745cf38b0c746c9a96c873a
unknown <alwyyourbae@gmail.com>
2024-05-17 22:04:26 +0700

    fix: fix bug

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,4 +1,11 @@
+import { ClientError, ServerError } from "../utils/error";
+
 const errorMiddleware = (err, req, res, next) => {
+  if (err & !(err instanceof ClientError)) {
+    err = new ServerError();
+  }
+
+  console.log(err);
   return res.status(err.status).json({ message: err.message });
 };
 
