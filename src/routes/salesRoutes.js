71caa29f1ffca1fbeac25ed5f6931592257227b7
unknown <alwyyourbae@gmail.com>
2024-05-17 22:50:25 +0700

    feat: add updateSaleById route

--- src/routes/salesRoutes.js
+++ src/routes/salesRoutes.js
@@ -4,6 +4,7 @@ import salesController from "../controller/salesController";
 const router = express.Router();
 
 router.post("/", salesController.createSale);
+router.put("/:id", salesController.updateSaleById);
 router.get("/", salesController.getAllSales);
 
 export default router;
