e4c3e9310a6cc5b21c471e1a053223c91516ce29
unknown <alwyyourbae@gmail.com>
2024-05-17 22:49:41 +0700

    feat: add updateSaleById

--- src/model/salesModel.js
+++ src/model/salesModel.js
@@ -5,8 +5,12 @@ const createSale = (newSale) => {
   return prisma.sales.create({ data: newSale });
 };
 
+const updateSaleById = (id, filteredData) => {
+  return prisma.sales.update({ where: { id }, data: filteredData });
+};
+
 const getAllSales = () => prisma.sales.findMany();
 
-const salesModel = { createSale, getAllSales };
+const salesModel = { createSale, updateSaleById, getAllSales };
 
 export default salesModel;
