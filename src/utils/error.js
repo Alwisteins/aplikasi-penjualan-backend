49f9b7096828e9d4c7cff97daea0874d28e5e2e5
unknown <alwyyourbae@gmail.com>
2024-05-17 22:02:06 +0700

    refactor: add custom error ClientError & ServerError

--- src/utils/error.js
+++ src/utils/error.js
@@ -1,24 +1,13 @@
-class ClientError extends Error {
-  constructor(message = "Terjadi kesalahan pada sisi klien", status = 400) {
-    super(message);
-    this.status;
-  }
-
-  setStatus(newStatus) {
-    this.status = newStatus;
-  }
-}
-
-class ServerError extends Error {
+export class ServerError extends Error {
   constructor(message = "Terjadi kesalahan pada sisi server", status = 500) {
     super(message);
-    this.status;
-  }
-
-  setStatus(newStatus) {
-    this.status = newStatus;
+    this.name = this.constructor.name;
+    this.status = status;
   }
 }
 
-const customError = { ClientError, ServerError };
-export default customError;
+export class ClientError extends ServerError {
+  constructor(message = "Terjadi kesalahan pada sisi klien", status = 400) {
+    super(message, status);
+  }
+}
