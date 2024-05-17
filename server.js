243c14edbcfd57d864ff414970b912a089e013bc
unknown <alwyyourbae@gmail.com>
2024-05-17 11:25:49 +0700

    refactor: change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from "./api";
 
-const PORT = process.env.PORT || 88;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log("SERVER RUNNING ON PORT " + PORT);
